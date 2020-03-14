6ef3c1ffd7aac5eff28a72b79a9fa7d1c599b972
Sam Elie <samradelie@gmail.com>
2020-03-14 12:16:52 -0700

    vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import VueCompositionApi from '@vue/composition-api'
+
+Vue.use(VueCompositionApi)
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
